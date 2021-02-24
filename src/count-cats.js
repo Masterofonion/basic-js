c372d526e3bbbb00f65498b013909e64e308b530
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-24 18:57:56 +0300

    fix: add solution for count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats (matrix) {
+
+    let row = matrix.reduce(function(preItem, item) {
+        return preItem.concat(item);             
+    }, []);  
+    return row.reduce(function(preItem, item){
+        if (item == '^^') {
+            console.log(item);
+            return preItem +=1;
+        } else {
+            return preItem;
+        }
+    }, 0);
+
 };
