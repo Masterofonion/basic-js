02cc9ac34d8b3c1a068fb9234baab87f7478e35c
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-24 19:19:05 +0300

    fix: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+  let res = arr;
+  let sum = [];
+  if (!Array.isArray(arr)) {
+      throw Error;
+  }
+  for (let i = 0; i < res.length; i++) {
+      
+      if (res[i] == '--discard-next') {
+          i++;        
+      } else if (res[i] == '--double-prev' && res[i-2] != '--discard-next' && i > 0) {
+          sum.push(res[i-1]);  
+      } else if ((res[i] == '--double-prev' && res[i-2] == '--discard-next') || 
+                 (res[i] == '--double-prev' && i == 0)) {
+          continue;    
+      } else if (res[i] == '--discard-prev' && res[i-2] != '--discard-next' && i > 0) {  
+          sum.pop();        
+      } else if ((res[i] == '--discard-prev' && res[i-2] == '--discard-next') || 
+                 (res[i] == '--discard-prev' && i == 0)) {
+          continue;
+      }
+      else if (res[i] == '--double-next' && i != res.length-1) {  
+          sum.push(res[i+1]);
+      } else if (res[i] == '--double-next' && i == res.length-1) {
+          continue;          
+      }
+      else {
+          sum.push(res[i]);
+      }
+      console.log(sum);
+  }
+    
+  
+  return sum;
 };
+
+//console.log(transform([1, 2, 3, '--discard-next']));
