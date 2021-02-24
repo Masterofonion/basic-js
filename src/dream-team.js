95fa65aa4b4306cf870af4c0b11f747b3fc61afc
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-24 19:02:57 +0300

    fix: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    
+    let sorted = members.filter(function (item) {
+        console.log(typeof item);
+        return (typeof item === 'string');
+    });
+    
+    return sorted.map(function(item) {
+        
+        item = item.trim().toUpperCase();
+        return item = item[0];   
+    }).sort().join('');
   // remove line with error and write your code here
-};
+}
+
+
+//console.log(isWrongType);
+
+
+
+
+
+
