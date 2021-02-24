673751a344e3a648dd4ed067192b33c7a5f6dba8
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-24 19:04:37 +0300

    fix: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let defSep = '+';
+    let defAddSep = '|';
+    let cell;
+    if (options.hasOwnProperty('separator')) {
+        defSep = options.separator;   
+    }
+    
+    if (options.hasOwnProperty('additionSeparator')) {
+        defAddSep = options.additionSeparator;   
+    }
+    
+    if (options.hasOwnProperty('addition')) {
+        let addition = options.addition + defAddSep;
+        let fullAddition = Array(options.additionRepeatTimes).fill(addition).join('');
+        fullAddition = fullAddition.slice(0,fullAddition.length - defAddSep.length);
+        cell = str + fullAddition + defSep;
+    } else {
+        cell = str + defSep;
+    }
+    
+    
+    
+    
+    let fullStr = Array(options.repeatTimes).fill(cell).join('');
+    fullStr = fullStr.slice(0,fullStr.length - defSep.length);
+    console.log(fullStr);
+    return fullStr;
 };
-  
+
+//repeater('la', { repeatTimes: 3 });
